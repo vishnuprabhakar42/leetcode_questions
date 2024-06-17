90f0ec96cf2a67b7e9b1f129725c2739cedbc363
vishnuprabhakar42 <63367697+vishnuprabhakar42@users.noreply.github.com>
2024-06-17 13:51:36 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+         for(long a = 0; a*a <= c; a++) {
+            for(long b = 0; b*b <= c; b++) {
+                if(a*a + b*b == c)
+                    return true;
+            }
+        }
+
+        return false;
+    
+        
+    }
+};
